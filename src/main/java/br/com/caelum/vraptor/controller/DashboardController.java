62b8a51d4d58dbcc4d72695eb822bd5921863292
Tiago Luz <tiagoluz.h607@gmail.com>
2020-10-01 18:29:59 -0300

    aula 05 - ok

--- src/main/java/br/com/caelum/vraptor/controller/DashboardController.java
+++ src/main/java/br/com/caelum/vraptor/controller/DashboardController.java
@@ -3,6 +3,8 @@ package br.com.caelum.vraptor.controller;
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.model.Usuario;
 
 @Path("dashboard")
 @Controller
@@ -12,4 +14,12 @@ public class DashboardController {
 	public void dashboard() {
 		
 	}
+	
+	@Post("salvausuario")
+	public void salvaUsuario(Usuario usuario) {
+		//validar usuario
+		//salvar no banco 
+		//direcionar para o dashboard
+	}
 }
+
