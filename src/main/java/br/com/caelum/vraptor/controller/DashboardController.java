2150b85ec2d5347d569df45eaa10f8c7cbb0b716
Tiago Luz <tiagoluz.h607@gmail.com>
2020-10-02 14:46:01 -0300

    Aula 06 - concluida persistindo usuario no banco de dados

--- src/main/java/br/com/caelum/vraptor/controller/DashboardController.java
+++ src/main/java/br/com/caelum/vraptor/controller/DashboardController.java
@@ -1,14 +1,21 @@
 package br.com.caelum.vraptor.controller;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.model.Usuario;
 
 @Path("dashboard")
 @Controller
 public class DashboardController {
+	
+	@Inject EntityManager em;
+	@Inject Result result;
 
 	@Get("")
 	public void dashboard() {
@@ -18,8 +25,11 @@ public class DashboardController {
 	@Post("salvausuario")
 	public void salvaUsuario(Usuario usuario) {
 		//validar usuario
-		//salvar no banco 
+		//salvar usuario no banco 
+		em.persist(usuario);
+		
 		//direcionar para o dashboard
+		result.redirectTo(DashboardController.class).dashboard();
 	}
 }
 
--- src/main/java/br/com/caelum/vraptor/model/Usuario.java
+++ src/main/java/br/com/caelum/vraptor/model/Usuario.java
@@ -1,8 +1,28 @@
 package br.com.caelum.vraptor.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
 public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotEmpty @Size(min = 4, max=20)
 	private String nome;
+	
+	@NotEmpty @Email
 	private String email;
+	
+	@NotEmpty @Size(min = 6, max=20)
 	private String senha;
 	public String getNome() {
 		return nome;
@@ -26,6 +46,15 @@ public class Usuario {
 	public void setSobrenome(String sobrenome) {
 		this.nome += " "+sobrenome;
 	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
 	
 	
 }
